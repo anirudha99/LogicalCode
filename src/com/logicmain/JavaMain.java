5fbcc2dc469965a6c0c19139deb8fe29f6d369bc
anirudha mayya <anirudha.mayya@gmail.com>
2021-08-26 08:39:21 +0530

    Updated coupon code

--- src/com/logicmain/JavaMain.java
+++ src/com/logicmain/JavaMain.java
@@ -2,6 +2,8 @@ package com.logicmain;
 
 import com.logicprogram.*;
 
+import Junitprogram.*;
+
 public class JavaMain {
 
 	public static void main(String[] args) {
@@ -15,6 +17,10 @@ public class JavaMain {
 		//ReverseNumber.reverseNumber();
 		//CouponNumber.couponNumber();
 		//StopWatch.stopwatchmethod();
+		
+		//-------JUnit Programs--------
+		//Temperature.temperaturemethod();
+		//MonthPayment.emimethod(args);
 
 	}
 
